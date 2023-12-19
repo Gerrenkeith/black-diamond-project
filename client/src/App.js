b7753dc9117e36bc448d001fe886b3572c7c237b
Gerren Keith <gerren.keith23@gmail.com>
2023-12-19 12:12:05 -0600

    added a feature that creates pages for each item in the items array using <Router>. Feature starts in App.js, ItemPage.js, and Shop.js  to have the ability to link each individual item to its own page from its card.

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,26 @@ import "./App.css"
 import Home from './Home';
 import NavBar from './NavBar';
 import Shop from './Shop';
+import ItemPage from './ItemPage';
 
 
-function App() {
+function App() {  
   console.log('App component rendered');
+
+  const items = [
+    { id: 1, name: 'item 1', price: '$300' },
+    { id: 2, name: 'Item 2', price: '$300' },
+    { id: 3, name: 'Item 3', price: '$300' },
+    { id: 4, name: 'Item 4', price: '$300' },
+    { id: 5, name: 'Item 5', price: '$300' },
+    { id: 6, name: 'Item 6', price: '$300' },
+    { id: 7, name: 'Item 7', price: '$300' },
+    { id: 8, name: 'Item 8', price: '$300' },
+    { id: 9, name: 'Item 9', price: '$300' },
+    { id: 10, name: 'Item 10', price: '$300' },
+    { id: 11, name: 'Item 11', price: '$300' },
+];
+
   return (
     <div className="App">
       <Router>
@@ -17,8 +33,14 @@ function App() {
             <Home />
           </Route>
           <Route exact path="/shop">
-            <Shop />
+            <Shop items={items}/>
           </Route>
+          {items.map((item) => (
+            <Route key={item.id} path={`/item/${item.id}`}>
+              {/* Render the ItemDetail component and pass the current item */}
+              <ItemPage item={item} />
+            </Route>
+          ))}
         </Switch>
       </Router>
     </div>
--- client/src/Shop.js
+++ client/src/Shop.js
@@ -1,27 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ShopRows from "./ShopRows";
 
-function Shop () {
-
-    const [showContent, setShowContent] = useState(true);
-
-  const toggleContent = () => {
-    setShowContent(!showContent);
-  };
-
-    const items = [
-        { id: 1, name: 'item 1', price: '$300' },
-        { id: 2, name: 'Item 2', price: '$300' },
-        { id: 3, name: 'Item 3', price: '$300' },
-        { id: 4, name: 'Item 4', price: '$300' },
-        { id: 5, name: 'Item 5', price: '$300' },
-        { id: 6, name: 'Item 6', price: '$300' },
-        { id: 7, name: 'Item 7', price: '$300' },
-        { id: 8, name: 'Item 8', price: '$300' },
-        { id: 9, name: 'Item 9', price: '$300' },
-        { id: 10, name: 'Item 10', price: '$300' },
-        { id: 11, name: 'Item 11', price: '$300' },
-    ];
+function Shop (items) {
 
     const groupedItems = Array.from({ length: Math.ceil(items.length / 5)}, (_, index) => 
 
@@ -34,7 +14,7 @@ function Shop () {
         
         <div className="Shop"> 
             {groupedItems.map((group, index) => ( 
-                <ShopRows key={index} items={group} />
+                <ShopRows key={index} items={group}/>
             ))}
              <section className="stamp">
                 <h3>©️ Black Diamond Dynamics</h3>
